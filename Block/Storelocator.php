0a007367a9fde8fbe2ebaba96ed25660c0c5710e
Krzysztof Kurzydło <krzysztof.kurzydlo@creativestyle.pl>
2019-07-03 13:48:39 +0200

    Initial commit after repo reset

---
+++ Block/Storelocator.php
@@ -0,0 +1,7 @@
+<?php
+namespace MageSuite\StoreLocator\Block;
+
+class StoreLocator extends \Magento\Framework\View\Element\Template
+{
+    protected $_template = 'MageSuite_StoreLocator::storelocator.phtml';
+}
---
+++ Controller/Index/Index.php
@@ -0,0 +1,25 @@
+<?php
+namespace MageSuite\StoreLocator\Controller\Index;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $pageFactory;
+
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $pageFactory
+    )
+    {
+        parent::__construct($context);
+        $this->pageFactory = $pageFactory;
+    }
+
+
+    public function execute()
+    {
+        return $this->pageFactory->create();
+    }
+}
