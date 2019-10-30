0f411e3f17766a78b33c816ec7282d85de848167
Błażej Dołęska <blazej.doleska@creativestyle.pl>
2019-10-30 10:00:51 +0000

    added functionality to set meta title and description for storelocator page

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -7,19 +7,35 @@ class Index extends \Magento\Framework\App\Action\Action
      * @var \Magento\Framework\View\Result\PageFactory
      */
     protected $pageFactory;
+    /**
+     * @var \MageSuite\StoreLocator\Helper\Configuration
+     */
+    protected $configuration;
 
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
-        \Magento\Framework\View\Result\PageFactory $pageFactory
+        \Magento\Framework\View\Result\PageFactory $pageFactory,
+        \MageSuite\StoreLocator\Helper\Configuration $configuration
     )
     {
         parent::__construct($context);
         $this->pageFactory = $pageFactory;
+        $this->configuration = $configuration;
     }
 
 
     public function execute()
     {
-        return $this->pageFactory->create();
+        $result = $this->pageFactory->create();
+
+        if (!empty($this->configuration->getMetaTitle())) {
+            $result->getConfig()->getTitle()->set($this->configuration->getMetaTitle());
+        }
+
+        if (!empty($this->configuration->getMetaDescription())) {
+            $result->getConfig()->setDescription($this->configuration->getMetaDescription());
+        }
+
+        return $result;
     }
 }
