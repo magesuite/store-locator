af9110336fc54ea5de6d9ad366c1406fd5f5f28e
Dariusz Matoga <dariusz.matoga@creativestyle.pl>
2021-04-09 08:03:33 +0000

    feat: [KRG-1689] Add possibility to disable store locator page per store view

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace MageSuite\StoreLocator\Controller\Index;
 
 class Index extends \Magento\Framework\App\Action\Action
@@ -7,25 +8,41 @@ class Index extends \Magento\Framework\App\Action\Action
      * @var \Magento\Framework\View\Result\PageFactory
      */
     protected $pageFactory;
+
     /**
      * @var \MageSuite\StoreLocator\Helper\Configuration
      */
     protected $configuration;
 
+    /**
+     * @var \Magento\Store\Model\StoreManager
+     */
+    protected $storeManager;
+
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
         \Magento\Framework\View\Result\PageFactory $pageFactory,
+        \Magento\Store\Model\StoreManager $storeManager,
         \MageSuite\StoreLocator\Helper\Configuration $configuration
-    ) {
+    )
+    {
         parent::__construct($context);
         $this->pageFactory = $pageFactory;
+        $this->storeManager = $storeManager;
         $this->configuration = $configuration;
     }
-
 
     public function execute()
     {
         $result = $this->pageFactory->create();
+
+        if (!$this->configuration->isEnabled()) {
+            $baseUrl = $this->storeManager->getStore()->getBaseUrl();
+            $resultRedirect = $this->resultRedirectFactory->create();
+            $resultRedirect->setUrl($baseUrl);
+
+            return $resultRedirect;
+        }
 
         if (!empty($this->configuration->getMetaTitle())) {
             $result->getConfig()->getTitle()->set($this->configuration->getMetaTitle());
